101b8700cdcee553d91d48dafe191bf256feaeaf
Huang Grace <huang.yahui.grace@gmail.com>
2019-11-30 11:47:01 +0800

    use Assets Image Set

--- HelloImage/ViewController.swift
+++ HelloImage/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let path = Bundle.main.path(forResource: "girl", ofType: "jpg") {
-            print("path: \(path)")
-            myImageView.image = UIImage.init(contentsOfFile: path)
-        }
+//        if let path = Bundle.main.path(forResource: "girl", ofType: "jpg") {
+//            print("path: \(path)")
+//            myImageView.image = UIImage.init(contentsOfFile: path)
+//        }
+        myImageView.image = UIImage(named: "mypic1")
     }
 
 
